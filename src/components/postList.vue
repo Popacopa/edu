fba0377244b8693b4e58fb094b74092bb0462e98
pavlider <118076297+Popacopa@users.noreply.github.com>
2023-08-11 09:12:37 +0300

    .

--- src/components/postList.vue
+++ src/components/postList.vue
@@ -1,12 +1,16 @@
 <template>
-    <div v-for="post in posts" class="post" :key="post.id">
-        <div class="title"><strong>{{ post.title }}</strong></div>
-        <div class="body">{{ post.body }}</div>
+    <div class="">
+        <h4>Список пользователей</h4>
+        <postItem :post="post" v-for="post in posts" :key="post.id"/>
     </div>
 </template>
 
 <script>
+import postItem from '@/components/postItem.vue'
     export default {
+        components: {
+            postItem
+        },
         props: {
             posts: {
                 type: Array,
@@ -16,23 +20,5 @@
 </script>
 
 <style lang="scss" scoped>
-    .post {
-        border-radius: 20px;
-        padding-left: 20px;
-        height: 10vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-top: 14px;
-        font-family: 'Montserrat', sans-serif;
-        background-color: rgba(255, 255, 255, 0.398);
-        transition: all 0.5s;
-        .title {
-            font-size: large;
-        }
-        &:hover {
-            box-shadow: 1px -1px 25px 0px rgba(64, 83, 97, 0.14);
-            transform: scale(1.1, 1.1);
-        }
-    }
+    
 </style>
